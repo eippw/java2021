5a5fa116f41bb32be1cca3a8f50a7f58e754a7f3
eippw <eippw@bpcsd.org>
2021-04-15 18:19:07 -0400

    adding a class to read in the file data into the proper arrays

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # B-P Java 2020-2021
 Location to share code for class of 2021
+
+Class Members who have accepted invite:
 Simon
 Cam
 Ivy
---
+++ ReadFiles.java
@@ -0,0 +1,13 @@
+public class ReadFiles {
+	private[] String tdata; 	// text data
+	private[][] double ndata;	// numeric data
+
+	public ReadFiles() {
+		;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
